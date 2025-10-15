2ffe2890deadbfe345ba5bfed0fd59c0be2b2548
Keerthi <2300030185@kluniversity.in>
2025-10-15 23:20:01 +0530

    modified pom.xml

--- src/main/java/com/klef/PracticedemoApplication.java
+++ src/main/java/com/klef/PracticedemoApplication.java
@@ -2,9 +2,10 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracticedemoApplication {
+public class PracticedemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticedemoApplication.class, args);
